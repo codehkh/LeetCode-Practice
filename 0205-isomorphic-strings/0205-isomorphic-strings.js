0c959312df41d3ad12f609b625473867c44480f3
khhong <118999470+codehkh@users.noreply.github.com>
2025-04-09 17:12:48 +0900

    Time: 5 ms (82.62%), Space: 55.1 MB (38.04%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if(s.length !== t.length) return false;
+    const hash = new Map();
+    const set = new Set();
+
+    for(let i = 0; i < s.length; i++){
+        if(hash.has(s[i])){
+            if(hash.get(s[i]) !== t[i]) return false;
+        } else {
+            if(set.has(t[i])) return false;
+            hash.set(s[i], t[i]);
+            set.add(t[i])
+        }
+    }
+
+    return true
+};
